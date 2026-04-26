0a36e565915082be5efd4cfb0a542d044899ca6d
nguyen6428882407-ux <nguyen6428882407@gmail.com>
2026-04-26 14:31:19 +0700

    yah
    super update

--- registerShipper.cpp
+++ registerShipper.cpp
@@ -1,5 +1,9 @@
-int Shipper_Management(shipper **headS);
+//OPTION 2
+int Shipper_Management(order**headO,shipper **headS);
 void registerShipper (shipper **headS);
+void deleteShipper (shipper **headS);
+void informationShipper (shipper **headS);
+void loadFileShipper(shipper **headS);
 
 static int N0S=1;
 //Mỗi 1 shipper là 1 Node chưa dc liên kết
@@ -47,21 +51,73 @@ shipper *CreateShipper () {
 
 void informationShipper (shipper **headS) {
     shipper *p=*headS;
-    FILE *S = fopen("information.txt","w");
+    FILE *S = fopen("Shipper_information.txt","w");
     if (S==NULL) {
         printf ("ERROR!");
         return;
     }
-    fprintf (S,"====================================List shipper==================================\n");
-    fprintf (S,"----------------------------------------------------------------------------------\n");
-    fprintf (S,"||%-30s||%-20s||%-10s||%-10s||\n","Name","CCCD","CODE","Priority");
+    fprintf (S,"%-30s||%-20s||%-10s||%-10s||%-6s||%-6s||%-6s||%-16s||%-6s||\n","Name","CCCD","CODE","Priority","Weight","x","y","Number of Orders","Status");
     while (p!=NULL) {
-        fprintf (S,"||%-30s||%-20lld||%-10s||%-10d||\n",p->Name,p->CCCD,p->code,p->prioritySP);
+        fprintf (S,"%-30s||%-20lld||%-10s||%-10d||%-6.2lf||%-6d||%-6d||%-16d||%-6d||\n",p->Name,p->CCCD,p->code,p->prioritySP,p->weight,p->x,p->y,p->numberOrder,p->status);
         p=p->next;
     }
     fclose(S);
 }
 
+//xoa khoang trang
+void trimRight(char *str){
+    int len = strlen(str); //strlen dung de dem do dai chuoi
+    while(len > 0 && str[len-1] == ' '){ // tai len-1 la tai ' '
+        str[len-1] = '\0'; // thay ' ' = \0
+        len--;// lui lai ktr tiep
+    }
+}
+
+void loadFileShipper(shipper **headS){ // ghi vao main tren selectoption
+	FILE *f = fopen("Shipper_information.txt", "r");
+	if(f==NULL) 
+        return;
+    shipper *temp;// giai phong list cu ( tranh tran bo nho )
+    while (*headS!=NULL){
+        temp=*headS;
+        *headS=(*headS)->next;
+        free(temp);
+    }
+    shipper *tail=NULL;
+    char buffer[1000];
+    fgets (buffer ,sizeof(buffer), f);//doc va bo qua dong dau
+    
+    char line[500];//doc tung dong -> sscanf de tach du lieu vao struct 
+    while (fgets(line,sizeof(line),f)){
+        shipper *newNode = (shipper*)malloc(sizeof(shipper));
+        if(newNode == NULL){
+            fclose(f);
+            return;
+        }
+        if (sscanf(line," %[^|]||%lld||%[^|]||%d||%lf||%d||%d||%d||%d||",//doc du lieu r gan vao struct
+            newNode->Name, &newNode->CCCD, newNode->code, &newNode->prioritySP, &newNode->weight, &newNode->x, &newNode->y
+            ,&newNode->numberOrder, &newNode->status)==9){
+                N0S++;// phai tang neu ko se co thag trung ma shipper
+                newNode->next=NULL;
+            }
+        else{
+            free(newNode);
+            continue;// bo qua vong lap nhay sang tiep theo khi sscanf !=9
+        }    
+        trimRight(newNode->Name); // dung cho ten
+        trimRight(newNode->code);// ma shipper
+        if (*headS==NULL){
+            *headS=newNode;
+            tail=newNode;
+        }
+        else {
+            tail->next=newNode;
+            tail=newNode;
+        }
+    }
+	fclose(f);
+	printf("======DONE LOAD FILE SHIPPER=====\n");
+}
 void registerShipper (shipper **headS) {
     shipper *Tail = *headS;
     while (Tail && Tail->next != NULL) {
@@ -124,7 +180,7 @@ void deleteShipper (shipper **headS) {
     informationShipper (headS);
 }
 
-int Shipper_Management(shipper **headS) {
+int Shipper_Management(order** headO,shipper **headS) {
     int countChoice = 0;
     int choiceTwo;
     printf("\n1. Register as a new Shipper"
@@ -146,7 +202,7 @@ int Shipper_Management(shipper **headS) {
         deleteShipper(headS);
         break;
     case 3:
-    	system("notepad information.txt");
+    	system("notepad Shipper_information.txt");
     	break;
     }
 }
